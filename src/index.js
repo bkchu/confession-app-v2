32bb11b12f8d18004aab69704839d5b228ae07cd
Brandon Chung <brandon.chung@dialexa.com>
2021-12-29 15:12:28 -0600

    add PWA support

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createStore } from "redux";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import reducer from "./redux/reducer";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const store = createStore(reducer);
 
@@ -19,6 +20,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
